2de32756301b6c0e66d1db97a2dd34f524cf711d
Ivan Maltsev <ivan.maltsev78@gmail.com>
2018-06-06 06:14:03 +0500

    Connected socket server and client

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.port || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Has server starten...')
 });
+
+io.on('connection', () => {
+    console.log('Server socket connected!');
+})
+
